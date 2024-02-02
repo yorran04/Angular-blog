281baf84708b5b6853557695039e57fd247db8ac
yorran04 <yorran_alves04@hotmail.com>
2024-02-02 11:02:15 -0300

    components dinamycs

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
 
+  @Input()
+  photoSmallCard:string=""
+  @Input()
+  titleSmallCard:string=""
+  
   constructor() { }
 
   ngOnInit(): void {
